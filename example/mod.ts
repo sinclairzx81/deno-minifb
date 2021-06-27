9e5836caf12c957c9e72a3f51312c5ebca946f9a
sinclairzx81 <haydn.developer@gmail.com>
2021-06-27 14:19:54 +0900

    math

--- example/mod.ts
+++ example/mod.ts
@@ -1,12 +1,12 @@
 
-import * as Delta from './engine/mod.ts'
+import * as Engine from './engine/mod.ts'
 import { Window } from '../lib/mod.ts'
 const window   = new Window({ width: 800, height: 600, x: 2000, topmost: true })
-const renderer = new Delta.Renderer(800, 600)
-const geometry = new Delta.BoxGeometry(10, 10, 10)
-const material = new Delta.Material(null as any)
-const mesh     = new Delta.Mesh(geometry, material)
-const scene    = new Delta.Scene()
+const renderer = new Engine.Renderer(800, 600)
+const geometry = new Engine.BoxGeometry(10, 10, 10)
+const material = new Engine.Material(null as any)
+const mesh     = new Engine.Mesh(geometry, material)
+const scene    = new Engine.Scene()
 
 let i = 0
 
