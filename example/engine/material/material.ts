73106dbabe77e076d1da77cf31cb87c2bce5fbc3
sinclairzx81 <haydn.developer@gmail.com>
2021-06-27 15:24:21 +0900

    math

--- example/engine/material/material.ts
+++ example/engine/material/material.ts
@@ -1,7 +1,16 @@
+import { Texture } from '../texture/mod.ts'
 import { Shader } from './shader.ts'
 
+export type Uniform = number | boolean | Texture
+
 export class Material {
+    public readonly uniforms: Map<string, Uniform>
+
     constructor(private readonly shader: Shader) {
+        this.uniforms = new Map<string, Uniform>()
+    }
+
+    public encode(encoder: GPUCommandEncoder) {
         
     }
 }
